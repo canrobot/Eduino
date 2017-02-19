757e1d18e7488d371458c242d003de49f869a118
shinkyungman <tlsrudak@naver.com>
2017-02-19 18:32:14 +0900

    IRremote 내용 변경

--- examples/7_IRremote/IRremote/IRremote.ino
+++ examples/7_IRremote/IRremote/IRremote.ino
@@ -9,24 +9,24 @@ void setup()
 {
    Serial.begin(9600);
    irrecv.enableIRIn(); // Start the receiver
-   pinMode(13, OUTPUT);//LED핀
+   pinMode(13, OUTPUT);//  LED
 }
 
 void loop() {
    if (irrecv.decode(&results)) {
       Serial.println(results.value, HEX);
       switch (results.value) {
-         case 0xFF18E7:    //2번을 눌렀을 때 '전진'
+         case 0xFF18E7:    //if you press the number "2", then "Forward"
               Motor_Left(CCW, 255);
               Motor_Right(CW, 255);
           break; 
           
-         case 0xFF38C7:    //5번을 눌렀을 때 '정지'
+         case 0xFF38C7:    //if you press the number "5", then "STOP"
               Motor_Left(CCW, 0);
               Motor_Right(CCW, 0);
           break;
 
-          case 0xFF4AB5:    //8번을 눌렀을 때 '후진'
+          case 0xFF4AB5:    //if you press the number "8", then "Backward"
               Motor_Left(CW, 255);
               Motor_Right(CCW, 255);
           break;
